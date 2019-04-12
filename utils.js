6430258f81e02a27cf9cbed3a889bdfb3eae6c5c
muhammad20 <muhammadahmad201196@hotmail.com>
2019-04-12 20:17:06 +0200

    extracted the utils to utils.js

---
+++ utils.js
@@ -0,0 +1,42 @@
+utils = {}
+var bigInt = require('big-integer')
+
+//convert Uint8Array to a single BigInt 
+utils.buf2bigInt = function(buf) {
+    var hex = [];
+    u8 = Uint8Array.from(buf);
+
+    u8.forEach(function (i) {
+        var h = i.toString(16);
+        if (h.length % 2) { h = '0' + h; }
+        hex.push(h);
+    });
+
+    return BigInt('0x' + hex.join(''));
+}
+
+// returns the greatest common divisor of x and y
+utils.gcd = function (x, y) {
+    var num_x = bigInt(x);
+    var num_y = bigInt(y);
+    if (num_x.equals(num_y)) return num_x;
+    if (num_x < num_y) {
+        while (num_x.notEquals(0)) {
+            var t = num_x;
+            num_x = num_y.mod(num_x);
+            num_y = t;
+        }
+        return num_y;
+    }
+    else {
+        while (num_y.notEquals(0)) {
+            var t = num_y;
+            num_y = num_x.mod(num_y);
+            num_x = t;
+        }
+        return num_x;
+    }
+}
+
+
+module.exports = utils;
