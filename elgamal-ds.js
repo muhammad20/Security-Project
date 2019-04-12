caacc892b55395c962696d295657c8aa793351c9
muhammad20 <muhammadahmad201196@hotmail.com>
2019-04-12 20:18:14 +0200

    removed the util functions to utils.js
    functions moved:
    - buf2bigInt(buf)
    - gcd(x,y)

--- elgamal-ds.js
+++ elgamal-ds.js
@@ -2,60 +2,7 @@
 
 var sha256 = require('fast-sha256');
 var bigInt = require('big-integer');
-
-var fs = require('fs');
-var largPrime = 'largePrime.txt';
-var q;
-
-async function getQ() {
-    var q;
-    fs.readFile(largPrime, 'utf8', function (err, contents) {
-        if (!err && contents) {
-            q = bigInt(contents.replace("\n", ""), 16);
-            console.log(q);
-            if (q.isPrime) console.log("alooo");
-        }
-    });
-    return q;
-}
-
-
-// returns the greatest common divisor of x and y
-gcd = function (x, y) {
-    var num_x = bigInt(x);
-    var num_y = bigInt(y);
-    if (num_x.equals(num_y)) return num_x;
-    if (num_x < num_y) {
-        while (num_x.notEquals(0)) {
-            var t = num_x;
-            num_x = num_y.mod(num_x);
-            num_y = t;
-        }
-        return num_y;
-    }
-    else {
-        while (num_y.notEquals(0)) {
-            var t = num_y;
-            num_y = num_x.mod(num_y);
-            num_x = t;
-        }
-        return num_x;
-    }
-}
-
-//convert Uint8Array to a single BigInt 
-function buf2bigInt(buf) {
-    var hex = [];
-    u8 = Uint8Array.from(buf);
-
-    u8.forEach(function (i) {
-        var h = i.toString(16);
-        if (h.length % 2) { h = '0' + h; }
-        hex.push(h);
-    });
-
-    return BigInt('0x' + hex.join(''));
-}
+var utils = require('./utils');
 
 /**
  * 
@@ -67,7 +14,7 @@ function buf2bigInt(buf) {
 async function elgamalDS(msg, q, a, x) {
     var hash = sha256.hash(msg);
 
-    var m = buf2bigInt(hash);
+    var m = utils.buf2bigInt(hash);
     console.log("message hash: " + m.toString(16) + "\n");
 
     var q_1 = bigInt(q).minus(1);
@@ -81,9 +28,9 @@ async function elgamalDS(msg, q, a, x) {
     //choose rand integer  1 < k < q-1
 
     var k = Math.floor(Math.random() * (10000000000 - 1) + 1);
-    console.log(k)
-    while (gcd(k, q_1) != 1) {
+    while (utils.gcd(k, q_1) != 1) {
         var k = Math.floor(Math.random() * (10000000000 - 1) + 1);
+        console.log(k);
     }
 
     console.log(k);
@@ -94,6 +41,7 @@ async function elgamalDS(msg, q, a, x) {
     console.log("K inverse is: " + kInv.toString(16) + "\n");
     console.log("s1 is: " + s1.toString(16) + "\n");
 
+    //compute s2:
     var s2 = bigInt(bigInt(kInv).multiply(bigInt(m).minus(bigInt(x).multiply(bigInt(s1))))).mod(q_1);
     if (s2.isNegative()) {
         s2 = bigInt(s2).add(q_1);
@@ -103,7 +51,6 @@ async function elgamalDS(msg, q, a, x) {
     return signature;
 }
 var globals = require('./globals');
-var msg = "sfsfhsdjfksdjkfhskjdfhksiwejriwejriowejroiwejroiewjriowejroiewjroiwejroiejroiejroiwejroiwejroiwejroiwejriowjerdfhsdkjf12402jalkfjslkfwe4oithqoirfjwelfnsdkjfnksjebngkjhskjdhkajsdhkashdahfskj";
 var x = bigInt("AB",16);
-console.log(elgamalDS("hamada", globals.q, globals.a, x));
+elgamalDS("hamada", globals.q, globals.a, x);
 
